3fb8706271f9dfd1f66e78a09fe8eb2677e9b8f2
kevindennyii <kevin.dennyii@gmail.com>
2020-08-17 01:22:10 -0500

    adding in login and loginUser view and controller

---
+++ controllers/loginUser.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+module.exports = (req, res) => {
+  // grab user name and password from request body
+  var { username, password } = req.body;
+
+  // find one user with the username, if user exist compare inputted password with password in mongo
+  User.findOne({ username: username }, (error, user) => {
+    if (user) {
+      // we use .compare instead of === to prevent a hacker trick called timing attack
+      bcrypt.compare(password, user.password, (error, same) => {
+        if (same) {
+          // if passwords match
+          // store user session
+          res.redirect("/");
+        } else {
+          res.redirect("/auth/login");
+        }
+      });
+    } else {
+      res.redirect("/auth/login");
+    }
+  });
+};
